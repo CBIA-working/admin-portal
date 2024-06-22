cdbad91f7c854faba4af1a7b944861cb3924b8a2
CBIA-working <tanmays2102@gmail.com>
2024-06-22 22:55:19 +0530

    tailwind

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 
 import { MenuItem } from 'primeng/api';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
 
-
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule,CommonModule,SidebarComponent,BreadcrumbModule],
+  imports: [RouterModule, CommonModule, SidebarComponent, BreadcrumbModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
   items: MenuItem[] = [];
   home: MenuItem = { icon: 'pi pi-home', routerLink: '/home' };
 
@@ -38,12 +37,19 @@ export class HeaderComponent  {
 
     // Add route-specific breadcrumbs
     if (url === '/managestudent') {
-      this.items.push({ label: 'Student Support > Manage Student', routerLink: '/managestudent' });
-    } 
-    if(url === '/studentservice') {
-      this.items.push({ label: 'Student Support > Student Service', routerLink: '/studentservice' });
-    } 
-    else if (url === '/admin') {
+      this.items.push({ 
+        label: 'Student Support  <i class="pi pi-angle-right"></i> Manage Student', 
+          escape: false,
+           routerLink: '/managestudent' 
+          });
+    } else if (url === '/studentservice') {
+      this.items.push({ 
+          label: 'Student Support <i class="pi pi-angle-right"></i> Student Service', 
+          escape: false, 
+          routerLink: '/studentservice' 
+      });
+  }
+   else if (url === '/admin') {
       this.items.push({ label: 'Admin', routerLink: '/admin' });
     }
     // Add more conditions for additional routes as needed
