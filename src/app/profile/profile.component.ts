3c4d91c178ec6331057319f8a042f5ad39c5137b
CBIA-working <tanmays2102@gmail.com>
2024-07-08 14:57:53 +0530

    editable fileds

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { TabViewModule } from 'primeng/tabview';
-import { AuthService } from '../authentication/auth.service';
+import { DialogModule } from 'primeng/dialog';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -12,28 +15,80 @@ import { AuthService } from '../authentication/auth.service';
     CardModule,
     AvatarModule,
     ButtonModule,
-    TabViewModule, // Import TabViewModule here
+    TabViewModule,
+    FormsModule,
+    DialogModule,
+    HttpClientModule
   ],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
-  user: any;
+export class ProfileComponent implements OnInit {
+  user: any = {
+    fname: '',
+    lname: '',
+    gender: '',
+    dob: '',
+    email: '',
+    dietaryPreference: '',
+    emergencyContactName: '',
+    emergencyContactNumber: '',
+    emergencyContactRelation: '',
+    address: '',
+    imageUrl: 'assets/avatar/1.jpg', // Default avatar
+    fullName: ''
+  };
+  originalUser: any;
+  showAvatarDialog: boolean = false;
+  showSaveButton: boolean = false;
+  saveSubscription: Subscription | null = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.user = JSON.parse(sessionStorage.getItem('user')!);
+    const userData = JSON.parse(sessionStorage.getItem('user')!);
+    if (userData) {
+      this.user = { ...userData };
+      this.originalUser = { ...userData };
+      this.user.fullName = `${userData.fname} ${userData.lname}`;
+    }
   }
-// Function to determine the avatar URL based on gender
-getAvatarUrl(gender: string): string {
-  switch (gender.toLowerCase()) {
-    case 'male':
-      return 'assets/avatar/male.jpg';
-    case 'female':
-      return 'assets/avatar/female.jpg';
-    default:
-      return 'assets/avatar/other.jpg';
+
+  updateFullName(): void {
+    const names = this.user.fullName.split(' ');
+    this.user.fname = names[0] || '';
+    this.user.lname = names.slice(1).join(' ') || '';
+    this.checkForChanges();
   }
-}
+
+  openAvatarDialog(): void {
+    this.showAvatarDialog = true;
+  }
+
+  selectAvatar(avatar: string): void {
+    this.user.imageUrl = avatar;
+    this.showAvatarDialog = false;
+    this.checkForChanges();
+  }
+
+  checkForChanges(): void {
+    this.showSaveButton = JSON.stringify(this.user) !== JSON.stringify(this.originalUser);
+  }
+
+  saveChanges(): void {
+    if (this.saveSubscription) {
+      this.saveSubscription.unsubscribe();
+    }
+    const payload = { ...this.user, image: this.user.imageUrl };
+    delete payload.imageUrl;
+
+    this.saveSubscription = this.http.post('http://localhost:3000/api/updateProfile', payload)
+      .subscribe(response => {
+        console.log('Profile updated successfully:', response);
+        this.originalUser = { ...this.user };
+        this.showSaveButton = false;
+      }, error => {
+        console.error('Error updating profile:', error);
+      });
+  }
 }
